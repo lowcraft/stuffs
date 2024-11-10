8b65edbbb256adcaf665604f6cd29f57a2bb834c
boa <boa@weboa.org>
2024-11-10 18:18:51 +0100

    fix bug 1

--- rndwp/rndwp.sh
+++ rndwp/rndwp.sh
@@ -5,16 +5,22 @@
 # description: Change randomly wallpaper on Gnome Desktop
 # author: boa <boa@weboa.org>
 # date: 2024/11/10
-# version: 0.1
+# version: 0.2
 # bugs: 
-# 1	sometime, black screen appear, must fix it!
 # 2	no check of the current wallpaper used 
+# fixed:
+# 1	sometime, black screen appear, must fix it!
+#	change random init number
+# mod:
+# 3	change (temporary) Gnome notification
 
 _path=$HOME/Images/Wallpapers
 _files=( $(ls $_path | sort -R) )
 _max=${#_files[@]}
-_random=$((1 + RANDOM % $_max))
+_random=$((0 + RANDOM % $_max))
 _image=${_files[_random]}
+_tmp=`gsettings get org.gnome.desktop.background picture-uri | tr -d "'"`
+_current=`basename $_tmp`
 
 gsettings set org.gnome.desktop.background picture-uri "file://$_path/$_image"
-notify-send "Change wallpaper to ${_image}" -a "Wallpaper Randomize Change" -t 200 -w
+# notify-send "Change wallpaper to ${_image}" -a "Wallpaper Randomize Change" -t 200 -w # 3
